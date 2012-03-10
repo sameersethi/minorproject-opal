699a465818403fa82cc05b69ab0c89f9193e6d26
Sameer Sethi <sameer.sethi@yahoo.com>
2012-03-10 10:18:54 +0530

    Initial Commit

---
+++ src/win32/dllmain.cxx
@@ -0,0 +1,50 @@
+/*
+ * dllmain.cxx
+ *
+ * DLL main entry point for OpenH323.dll
+ *
+ * Open H323 Library
+ *
+ * Copyright (c) 1993-1998 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Open H323 Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Revision: 19279 $
+ * $Author: rjongbloed $
+ * $Date: 2008-01-16 22:08:34 -0600 (Wed, 16 Jan 2008) $
+ */
+
+#include <ptlib.h>
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef _WIN32_WCE
+HINSTANCE PDllInstance;
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID)
+#else
+HANDLE PDllInstance;
+BOOL WINAPI DllMain(HANDLE hinstDLL, DWORD fdwReason, LPVOID)
+#endif
+{
+  if (fdwReason == DLL_PROCESS_ATTACH)
+    PDllInstance = hinstDLL;
+  return TRUE;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
